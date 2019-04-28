6ab5b8aabe9480bc358da865fbe6d7af9a7bb799
rgunning <rgunning@kth.se>
2019-04-28 22:47:05 +0200

    added some more drinks and ingredients

--- drinks.py
+++ drinks.py
@@ -46,6 +46,40 @@ drink_list = [
 			"tequila": 50,
 			"oj": 150
 		}
+	}, {
+		"name": "Grogg",
+		"ingredients": {
+			"vodka": 50,
+			"coke": 150
+		}
+	}, {
+		"name": "Appletini",
+		"ingredients": {
+			"vodka": 40,
+			"appschnaps": 15,
+			"trisec": 15
+		}
+	}, {
+		"name": "White Lady",
+		"ingredients": {
+			"gin": 50,
+			"trisec": 25,
+			"lemon": 25
+		}
+	}, {
+		"name": "Sex on the beach",
+		"ingredients": {
+			"vodka": 25,
+			"peachschnaps": 25,
+			"oj": 40,
+			"cj": 20
+		}
+	}, {
+		"name": "Caribbean Sunrise",
+		"ingredients": {
+			"rum": 45,
+			"oj": 60
+		}
 	}
 ]
 
@@ -54,8 +88,12 @@ drink_options = [
 	{"name": "Rum", "value": "rum"},
 	{"name": "Vodka", "value": "vodka"},
 	{"name": "Tequila", "value": "tequila"},
+	{"name": "Lemon Juice", "value": "lemon"},
 	{"name": "Tonic Water", "value": "tonic"},
+	{"name": "Triple Sec", "value": "trisec"},
+	{"name": "Apple Schnaps", "value": "appschnaps"},
 	{"name": "Coke", "value": "coke"},
 	{"name": "Orange Juice", "value": "oj"},
+	{"name": "Cranberry Juice", "value": "cj"},
 	{"name": "Margarita Mix", "value": "mmix"}
 ]
